9561bd950df73aa99fb3bb1f6a13d49610b6078e
harloxx <chloe0105@naver.com>
2022-04-11 23:04:17 +0900

    delete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
+class App extends Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  getMovieData = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts.mx/api/v2/list_movies.json?sort_by=rating"
+    );
+    this.setState({ isLoading: false, movies });
+  };
+
+  async componentDidMount() {
+    this.getMovieData();
+  }
+
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span>Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map((data) => {
+              console.log(data);
+              return (
+                <Movie
+                  key={data.id}
+                  id={data.id}
+                  title={data.title}
+                  year={data.year}
+                  genres={data.genres}
+                  summary={data.summary}
+                  cover_img={data.medium_cover_image}
+                />
+              );
+            })}
+          </div>
+        )}
+      </section>
+    );
+  }
 }
 
 export default App;
